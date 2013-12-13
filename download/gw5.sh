e9e8cbe54603e77fe11f79b48b65ba918cf5f799
zwa <zwa@orsyp.com>
2013-12-13 17:08:35 -0500

    Changes to be committed: 	modified:   download/gw5.sh

--- download/gw5.sh
+++ download/gw5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-yum -y -q install ruby ruby-libs ruby-shadow
-rpm -ivh http://yum.puppetlabs.com/el/5/products/i386/puppetlabs-release-5-7.noarch.rpm
-yum -y -q install puppet facter
+yum -y -q install ruby ruby-libs ruby-shadow #-y: to always answer 'y'
+rpm -ivh http://yum.puppetlabs.com/el/5/products/i386/puppetlabs-release-5-7.noarch.rpm # to build puppet repository
+yum -y -q install puppet facter # -q to install quietly
 puppet module install jfryman/nginx
-curl -s -O 192.168.115.41/download/nginx.pp
+curl -s -O 192.168.115.41/download/nginx.pp  # -s: silent mode
 puppet apply nginx.pp
 /sbin/service iptables stop
